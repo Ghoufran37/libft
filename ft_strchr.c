c3f268aeb2d4e2b2a89fdb5ff6378ee1c06ed1fb
Ghufran Lakem IshQaidef <68250849+Ghoufran37@users.noreply.github.com>
2023-02-03 12:56:08 +0400

    Update ft_strchr.c
    out put: 
    String after (.) is : (.google.com)

--- ft_strchr.c
+++ ft_strchr.c
@@ -24,3 +24,15 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (0);
 }
+
+/*
+int main()
+{
+	char str[] = "http://www.google.com";
+	char ch = '.';
+	char *ret;
+
+	ret = ft_strchr(str, ch);
+	printf("String after (%c) is : (%s)\n", ch, ret);
+	return (0);
+}*/
