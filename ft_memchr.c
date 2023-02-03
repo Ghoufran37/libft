da4f334b60f8834c451785a0f6804656617a0c7c
Ghufran Lakem IshQaidef <68250849+Ghoufran37@users.noreply.github.com>
2023-02-03 12:26:15 +0400

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -26,3 +26,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+
+/*
+int main(void)
+{
+	char src[50] = "gooddaysall";
+	char *ret;
+
+	ret = ft_memchr(src, 's', 15);
+	printf("%s", ret);
+	return (0);
+}*/
